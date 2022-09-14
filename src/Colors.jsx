7707e25489e4577543c7c518c0bdb3240dd3cffd
NadyaCodes <nadyacorscadden@gmail.com>
2022-09-14 08:09:00 -0400

    add and alter colors

--- src/Colors.jsx
+++ src/Colors.jsx
@@ -2,10 +2,11 @@ import Options from "./Options"
 
 export default function Colors(props) {
   const {color, setColor} = props
-  const colorOptions = ['blue', 'green', 'purple']
+  const colorOptions = ['blue', 'green', 'purple', 'yellow', 'pink']
 
   const options = colorOptions.map((col, index) => {
     return <div key={index}><Options color={col} setColor={setColor}/></div>
   })
-  return(<div className='options-container'>{options}<h2>Current Color: <Options color={color} /></h2></div>)
+  
+  return(<div className='options-container'><span className='options'>{options}</span><span className='current-container'><h2>Current Color: &nbsp;</h2><Options color={color} /></span></div>)
 }
