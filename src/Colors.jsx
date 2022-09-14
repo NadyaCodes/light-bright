2e5f8bb6cd426d5a5925715a8a1e09403b483a33
NadyaCodes <nadyacorscadden@gmail.com>
2022-09-14 08:21:53 -0400

    add reset  button and display color picker

--- src/Colors.jsx
+++ src/Colors.jsx
@@ -7,6 +7,10 @@ export default function Colors(props) {
   const options = colorOptions.map((col, index) => {
     return <div key={index}><Options color={col} setColor={setColor}/></div>
   })
+
+  const reset = () => {
+    window.location.reload(false);
+  }
   
-  return(<div className='options-container'><span className='options'>{options}</span><span className='current-container'><h2>Current Color: &nbsp;</h2><Options color={color} /></span></div>)
+  return(<div className='options-container'><span className='options'>{options}</span><span className='current-container'><h2>Current Color: &nbsp;</h2><Options color={color} /></span> &nbsp; &nbsp;<button onClick={() => reset()} className='reset'>Reset</button></div>)
 }
